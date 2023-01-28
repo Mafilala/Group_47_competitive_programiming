79b2823c4309fb832aa4b9b3f9c1d1ebd4c120d9
Mafilala <marufsherefa9@gmail.com>
2023-01-28 21:35:17 +0300

    Create largestNumber.py

---
+++ largestNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def compare(n1, n2):
+            if int(str(n1) + str(n2)) < int(str(n2) + str(n1)):
+                return -1
+            else:
+                return 1
+        ans = sorted(nums,reverse=True, key=cmp_to_key(compare))
+        return str(int("".join(map(str,ans))))
