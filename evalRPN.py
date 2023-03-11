98e50ef9adf9c8ac6841474568377b996e6c00d6
Mafilala <marufsherefa9@gmail.com>
2023-03-11 16:26:21 +0300

    Create evalRPN.py

---
+++ evalRPN.py
@@ -0,0 +1,13 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        store = ["+", "-", "*", "/"]
+        for sy in tokens:
+            if sy not in store:
+                stack.append(sy)
+            else:
+                n2 = stack.pop()
+                n1 = stack.pop()
+                ans = int(eval(f"{n1} {sy} {n2}"))
+                stack.append(ans)
+        return int(stack.pop())
