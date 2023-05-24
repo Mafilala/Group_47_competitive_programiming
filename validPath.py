6e3ded41b71d9c3b54057b0fe941f80166bcb744
Mafilala <marufsherefa9@gmail.com>
2023-05-24 20:31:52 +0300

    Create validPath.py

---
+++ validPath.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        visited = set()
+        visited.add(source)
+        queue = deque([source])
+    
+        while queue:
+            curr_node = queue.popleft()
+            if curr_node == destination:
+                return True
+
+            for next_node in graph[curr_node]:
+                if next_node not in visited:
+                    visited.add(next_node)
+                    queue.append(next_node)
+        
+        return False
