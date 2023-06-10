54371fabfa4c1df5ab0fd3bc2f2a18883192d1b6
Mafilala <marufsherefa9@gmail.com>
2023-06-10 22:46:09 +0300

    Update mergeKLists.py

--- mergeKLists.py
+++ mergeKLists.py
@@ -7,29 +7,16 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         merged = ListNode()
         dummy = merged
-        heaps = []
-        limit = 0
-        for ll in lists:
-            heap = []
-            while ll:
-                limit += 1
-                heapq.heappush(heap, ll.val)
-                ll = ll.next
-            if heap:
-                heaps.append(heap)
-        l = len(heaps)
-        if heaps and heaps[0]:
-            min_heap = heaps[0]   
-        while limit:
-            for heap in heaps:
-                if not min_heap:
-                    min_heap = heap
-                if heap and heap[0] < min_heap[0]:
-                    min_heap = heap
-            if min_heap:
-                val = min_heap[0]
-                heapq.heappop(min_heap)
-                merged.next = ListNode(val)
-                merged = merged.next
-            limit -= 1
+        heap = []
+        for list_node in lists:
+            while list_node:
+                heappush(heap, list_node.val)
+                list_node = list_node.next
+        while heap:
+            val = heappop(heap)
+            merged.next = ListNode(val)
+            merged = merged.next
         return dummy.next
+        
+
+        
