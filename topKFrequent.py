08fafddf43aa5b1eac28535eb0b0e332d78598ac
Mafilala <marufsherefa9@gmail.com>
2023-06-10 23:18:11 +0300

    Create topKFrequent.py

---
+++ topKFrequent.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        words.sort()
+        ctr = Counter(words)
+        ans = []
+        for val, ct in ctr.most_common(k):
+            ans.append(val)
+        return ans
